5f26809f6653752a279eb6787a74e44ea887782d
achinyaev <achinyaev@gmail.com>
2021-04-23 19:51:40 +0300

    hw06

--- hw06/edu_parse/settings.py
+++ hw06/edu_parse/settings.py
@@ -19,15 +19,15 @@ LOG_LEVEL = "DEBUG"
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -64,11 +64,21 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+IMAGES_STORE = 'images'
+
 ITEM_PIPELINES = {
     'edu_parse.pipelines.EduParsePipeline': 300,
     'edu_parse.pipelines.EduParseMongoPipeline': 400,
+    'edu_parse.pipelines.EduImageDownloadPipeline': 200,
 }
 
+DOWNLOAD_DELAY = 0.05
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_MAX_DELAY = 10
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- hw06/edu_parse/spiders/avito.py
+++ hw06/edu_parse/spiders/avito.py
@@ -1,20 +1,23 @@
 import scrapy
 import pymongo
-
+from hw06.edu_parse.spiders.xpath import AVITO_KV_XPATH, AVITO_PG_XPATH
+from hw06.edu_parse.loaders import AvitoLoader
 
 class AvitoSpider(scrapy.Spider):
     name = 'avito'
     mongo_url = "mongodb://localhost:27017"
-    allowed_domains = ['avito.ru']
-    start_urls = ['https://www.avito.ru/krasnodar/kvartiry/prodam-ASgBAgICAUSSA8YQ?p=1']
+    allowed_domains = ['www.avito.ru']
+    #start_urls = ['https://www.avito.ru/krasnodar/kvartiry/prodam-ASgBAgICAUSSA8YQ?p=1']
+    #start_urls = ['https://www.avito.ru/krasnodar/nedvizhimost/']
+    start_urls = ['https://www.avito.ru/sankt-peterburg/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.dbclient = pymongo.MongoClient(self.mongo_url)
-        callbacks = {
-            "pagination": self.parse,
-            "kvartira": self.kv_parse,
-        }
+        # callbacks = {
+        #     "pagination": self.parse,
+        #     "kvartira": self.kv_parse,
+        # }
 
     @staticmethod
     def _get_follow_xpath(response, xpath, callback):
@@ -22,11 +25,12 @@ class AvitoSpider(scrapy.Spider):
             yield response.follow(link, callback=callback)
 
     def parse(self, response, *args, **kwargs):
+        callbacks = {"pagination": self.parse, "kvartira": self.kv_parse}
         for key, xpath in AVITO_PG_XPATH.items():
             yield from self._get_follow_xpath(response, xpath, self.callbacks[key])
 
     def kv_parse(self, response):
-        self.coll_name = "flat"
+        self.coll_name = "kvartira"
         loader = AvitoLoader(response=response)
         loader.add_value("url", response.url)
         for key, xpath in AVITO_KV_XPATH.items():
